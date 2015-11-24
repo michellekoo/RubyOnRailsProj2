839f71fdaeaa5dab2c745b6452db27e862324a4a
Truong Pham <phamcse@berkeley.edu>
2015-11-24 12:20:37 -0800

    add belongs-to and has-many to user, needed to organize and redesign layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,21 @@ class User < ActiveRecord::Base
 
     attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :user_name 
    
-   # has_many :achievements 
    # def achievements
    #    #this will also fix the error you see, although it's fundamentally incorrect
    # end
+
+
+  	validates :first_name, presence: true
+
+  	validates :last_name, presence: true
+
+  	validates :user_name, presence: true, uniqueness: true
+
+   	has_many :gratitudes
+
+   def full_name
+   		first_name + " " + last_name 
+   end
+
 end
