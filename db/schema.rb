b95960c6668ee30a7b97f717feb20178c6a35e63
Michelle <michellevkoo@yahoo.com>
2015-11-26 13:05:52 -0800

    Tried sqlite and pg

--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,6 @@ ActiveRecord::Schema.define(version: 20151121220949) do
   add_index "gratitudes", ["user_id"], name: "index_gratitudes_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "user_name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
